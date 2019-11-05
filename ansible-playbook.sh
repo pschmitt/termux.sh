41c05846d45a15339298d067882fa13c6936dc78
Philipp Schmitt <philipp@schmitt.co>
2019-11-05 15:24:23 +0100

    Use absolute paths instead of cd'ing

--- ansible-playbook.sh
+++ ansible-playbook.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
 
-cd "$(dirname "$(realpath "$0")")" || exit 9
-
-./ansible-proot.sh "ansible-playbook $*"
+"$(dirname "$(realpath "$0")")"/ansible-proot.sh \
+  "ansible-playbook $*"
--- ansible.sh
+++ ansible.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
 
-cd "$(dirname "$(realpath "$0")")" || exit 9
-
-./ansible-proot.sh "ansible $*"
+"$(dirname "$(realpath "$0")")"/ansible-proot.sh \
+  "ansible $*"
