22b9e18dab80a8f4fc2720d6b945a9ac4782f273
Philipp Schmitt <philipp@schmitt.co>
2020-05-14 07:28:49 +0200

    Avoid restarting the server at every call

--- scripts/termux_adb-self.sh
+++ scripts/termux_adb-self.sh
@@ -1,16 +1,25 @@
 #!/usr/bin/env bash
 
-cd "$(readlink -f "$(dirname "$0")")" || exit 9
+already_connected() {
+  adb devices | grep -qE "localhost|127.0.0.1"
+}
 
-# adb connect
-{
-  adb kill-server
+adb_connect() {
+  # Setup ADB over network
   adbd-network restart
-
   # Wait 5 seconds max for the ADB port
   timeout "${MAX_WAIT_TIME:-5}" bash -c -- \
     'while ! nc -w 1 -z localhost 5555; do sleep 1; done'
   adb connect localhost
-} &>/dev/null
+}
+
+# adb connect
+if ! already_connected
+then
+  {
+    adb kill-server
+    adb_connect
+  } >&2
+fi
 
 adb -s localhost "$@"
